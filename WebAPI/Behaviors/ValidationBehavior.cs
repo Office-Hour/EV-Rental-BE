7cffde7768ccb7af8b21221d9e149acba5d47338
nnh53 <nnh53.work@gmail.com>
2025-11-07 07:25:42 +0700

    fix: cai nay fix lai ok - need review
    (cherry picked from commit e2a7f3a195eff7d9900d7cdac1f80f5dfa0437ff)

--- WebAPI/Behaviors/ValidationBehavior.cs
+++ WebAPI/Behaviors/ValidationBehavior.cs
@@ -1,25 +1,33 @@
-﻿using FluentValidation;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
 
 namespace WebAPI.Behaviors
 {
-    public sealed class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest> validator)
+    public sealed class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
         : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
     {
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            if (validator == null)
+            if (!validators.Any())
             {
                 return await next(cancellationToken);
             }
 
             var context = new ValidationContext<TRequest>(request);
 
-            var errorsDictionary = validator.Validate(context).Errors;
+            var validationResults = await Task.WhenAll(
+                validators.Select(validator => validator.ValidateAsync(context, cancellationToken))
+            );
 
-            if (errorsDictionary.Count > 0)
+            var failures = validationResults.SelectMany(result => result.Errors).Where(failure => failure is not null).ToList();
+
+            if (failures.Count > 0)
             {
-                throw new ValidationException(errorsDictionary);
+                throw new ValidationException(failures);
             }
 
             return await next(cancellationToken);
