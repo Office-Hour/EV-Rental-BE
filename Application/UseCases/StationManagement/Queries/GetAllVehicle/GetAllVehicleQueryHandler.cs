37da96c3d56c7a8036f182666df9115c3fd1d172
ThanhTNV <thanhtran190304@gmail.com>
2025-11-11 22:14:23 +0700

    add vehicle at station id

--- Application/UseCases/StationManagement/Queries/GetAllVehicle/GetAllVehicleQueryHandler.cs
+++ Application/UseCases/StationManagement/Queries/GetAllVehicle/GetAllVehicleQueryHandler.cs
@@ -14,7 +14,21 @@ public class GetAllVehicleQueryHandler(IUnitOfWork uow, IMapper mapper) : IReque
         var vehicleRepo = uow.Repository<Vehicle>();
         var query = vehicleRepo.AsQueryable();
         var vehicles = await query.ToListAsync(cancellationToken);
+
+        var vehicleAtStationRepo = uow.Repository<VehicleAtStation>();
         var dtos = mapper.Map<List<VehicleDetailsDto>>(vehicles);
+        foreach (var vehicle in vehicles)
+        {
+            var vehicleAtStation = await vehicleAtStationRepo.AsQueryable()
+                .FirstOrDefaultAsync(vs => vs.VehicleId == vehicle.VehicleId, cancellationToken);
+            var dto = dtos.First(d => d.VehicleId == vehicle.VehicleId);
+            if (vehicleAtStation != null)
+            {
+                dto.VehicleAtStationId = vehicleAtStation.VehicleAtStationId;
+                dto.CurrentBatteryCapacityKwh = vehicleAtStation.CurrentBatteryCapacityKwh;
+                dto.Status = vehicleAtStation.Status;
+            }
+        }
         return dtos;
     }
 }
